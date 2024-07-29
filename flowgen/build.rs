5f9b83c076caf5cbf4f1ef9d42b1186a8e09bcb6
Jakub Dulny <jdulny@connve.com>
2024-07-29 23:30:41 +0200

    Added prost-build

--- flowgen/build.rs
+++ flowgen/build.rs
@@ -1,4 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut config = prost_build::Config::new();
+    config.protoc_arg("--experimental_allow_proto3_optional");
+
     // Google gRPC APIs.
     // https://github.com/googleapis/googleapis
     tonic_build::configure()
